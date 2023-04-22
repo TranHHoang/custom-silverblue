7e3733e37ba576b2d5d81e100695a6c81d056a68
TranHoang <tranhoang345@gmail.com>
2023-04-22 14:58:31 +0700

    Builds akmods-nvidia image and uses it in the custom image

--- nvidia/build.sh
+++ nvidia/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-set -ouex pipefail
+set -oeux pipefail
 
-RELEASE="$(rpm -E %fedora)"
+RELEASE="$(rpm -E '%fedora')"
+NVIDIA_PACKAGE_NAME="nvidia"
 
-# Setup
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
 
 # Fetch necessary rpms
@@ -12,29 +12,14 @@ wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm
 # Install rpms
-rpm-ostree install /tmp/rpms/*.rpm fedora-repos-archive # Get older kernel for NVidia
+rpm-ostree install /tmp/rpms/*.rpm # Get older kernel for NVidia
 
-# Fetch necessary repos
-wget -P /tmp/repos \
-    https://download.opensuse.org/repositories/home:lamlng/Fedora_37/home:lamlng.repo
-cp -r /tmp/repos/. /etc/yum.repos.d/
-
-# Modify base image
-REMOVED_PACKAGES=(
-    gnome-software gnome-software-rpm-ostree gnome-classic-session gnome-tour
-    firefox firefox-langpacks yelp toolbox
-)
-INSTALLED_PACKAGES=(
-    akmod-nvidia xorg-x11-drv-nvidia
-    gnome-tweaks zsh distrobox ibus-bamboo
-)
-rpm-ostree override remove ${REMOVED_PACKAGES[@]} $(printf -- "--install=%s " ${INSTALLED_PACKAGES[@]})
+rpm-ostree install akmod-${NVIDIA_PACKAGE_NAME} xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,devel,kmodsrc,power}* mock
 
 # alternatives cannot create symlinks on its own during a container build
 ln -s /usr/bin/ld.bfd /etc/alternatives/ld && ln -s /etc/alternatives/ld /usr/bin/ld
 
-# Setup NVidia
-NVIDIA_PACKAGE_NAME="nvidia"
+# Either successfully build and install the kernel modules, or fail early with debug output
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-${NVIDIA_PACKAGE_NAME}" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
 NVIDIA_LIB_VERSION="$(basename "$(rpm -q "xorg-x11-drv-${NVIDIA_PACKAGE_NAME}" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
@@ -44,3 +29,13 @@ akmods --force --kernels "${KERNEL_VERSION}" --kmod "${NVIDIA_PACKAGE_NAME}"
 
 modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/${NVIDIA_PACKAGE_NAME}/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
 (cat /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
+
+RELEASE="$(rpm -E '%fedora.%_arch')"
+cat <<EOF > /var/cache/akmods/nvidia-vars
+KERNEL_VERSION=${KERNEL_VERSION}
+RELEASE=${RELEASE}
+NVIDIA_PACKAGE_NAME=${NVIDIA_PACKAGE_NAME}
+NVIDIA_FULL_VERSION=${NVIDIA_FULL_VERSION}
+NVIDIA_AKMOD_VERSION=${NVIDIA_AKMOD_VERSION}
+NVIDIA_LIB_VERSION=${NVIDIA_LIB_VERSION}
+EOF
---
+++ silverblue/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+# Setup
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{cisco-openh264,modular,updates-modular}.repo
+
+# Fetch necessary rpms
+wget -P /tmp/rpms \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm
+# Install rpms
+rpm-ostree install /tmp/rpms/*.rpm # Get older kernel for NVidia
+
+# Fetch necessary repos
+wget -P /tmp/repos \
+    https://download.opensuse.org/repositories/home:lamlng/Fedora_37/home:lamlng.repo
+cp -r /tmp/repos/. /etc/yum.repos.d/
+
+# Modify base image
+source /var/cache/akmods/nvidia-vars
+REMOVED_PACKAGES=(
+    gnome-software gnome-software-rpm-ostree gnome-classic-session gnome-tour
+    firefox firefox-langpacks yelp toolbox
+)
+INSTALLED_PACKAGES=(
+    xorg-x11-drv-nvidia-{,power}*
+    /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/kmod-${NVIDIA_PACKAGE_NAME}-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm
+    gnome-tweaks zsh distrobox ibus-bamboo
+)
+rpm-ostree override remove ${REMOVED_PACKAGES[@]}
+rpm-ostree install ${INSTALLED_PACKAGES[@]}
+
+# alternatives cannot create symlinks on its own during a container build
+ln -s /usr/bin/ld.bfd /etc/alternatives/ld && ln -s /etc/alternatives/ld /usr/bin/ld
